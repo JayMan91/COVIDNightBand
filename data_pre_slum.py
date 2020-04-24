603c06ac5e2a9824c5bae5cf360e86b8f6d60aac
JayMan91 <jayantam2017@email.iimcal.ac.in>
2020-04-24 19:37:19 +0200

    slum btyuer

--- data_pre_slum.py
+++ data_pre_slum.py
@@ -17,20 +17,22 @@ for file in files:
 
 file_df['Total Population of Town'] = file_df['Total Population of Town'].astype(float)
 
-file_df_townaggregated = file_df.groupby(['State Code', 'State Name', 'District Code', 'District Name',
-                'Sub District Code','Sub District Name',
-                'Town Code','Town Name']).agg({'Total Population of Town':'mean',
+file_df_townaggregated = file_df.groupby(['State Code', 'State Name',
+#'District Name','Sub District Name','Town Name', 
+  'District Code','Sub District Code','Town Code']).agg({'Total Population of Town':'mean',
                 'Slum Population (approximate)':'sum'}).reset_index(drop = False)
 file_df_districtaggregated = file_df_townaggregated.groupby(['State Code', 'State Name', 
-	'District Code', 'District Name']).agg({'Total Population of Town':'sum',
+ # 'District Name',
+	'District Code']).agg({'Total Population of Town':'sum',
          'Slum Population (approximate)':'sum'}).reset_index(drop = False)
 file_df_districtaggregated.columns = ['State Code', 'State Name', 'District Code', 
-'District Name','Population of Town','Slum Population']
+'Population of Town','Slum Population']
 
 districts_df = pd.read_csv("CensusData/Districts.csv")
 districts_df = districts_df[['State Code', 'District Code','Name','Persons', 'Males', 'Females',
        'Area In sq. km', 'Population per sq. km.']]
-
+districts_df.rename(columns={'Name':'District Name',
+  'Persons':'District Population'}, inplace=True)
 slum_population = file_df_districtaggregated.merge(districts_df,on=['State Code',
-	'District Code'])
+	'District Code'],how='left')
 slum_population.to_csv("DistrictwiseSlumandTotalPopulation.csv",index=False)
